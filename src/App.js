d34ca5675803e08db8f4f554f61cc3b26a6aff57
Debangshu Nath <thecrusader.25225@gmail.com>
2024-07-09 00:38:28 +0530

    Updated CurrentLocationDetails.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import FiveDayForecastDetails from "./FiveDayForecastDetails";
 
 export default function App() {
   const API_KEY = "b82565c34cea20f860e1531e0d3a4597";
+  const AQI_API_KEY = "63814a94546ca970e99b547e46360b23338e4a9f";
 
   const [cityName, setCityName] = useState("");
   const [stateName, setStateName] = useState("");
   const [countryName, setCountryName] = useState("");
 
   const [locationSearchData, setLocationSearchData] = useState([]);
-
+  const [aqi, setAqi] = useState("");
   const [weatherData, setWeatherData] = useState("");
   const [bulkWeatherData, setBulkWeatherData] = useState([]);
   const [fiveDayForecast, setFiveDayForecast] = useState([]);
+  const [timeData, setTimeData] = useState("");
 
   const [loading, setLoading] = useState(false);
 
@@ -71,6 +73,13 @@ export default function App() {
         setLoading(false);
       });
   };
+  const fetchAQI = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((res) => setAqi(res));
+  };
 
   const handleClickLocation = (location) => {
     setCityName(location.name);
@@ -78,17 +87,21 @@ export default function App() {
     setCountryName(location.sys.country);
     fetchWeathermapData(location.coord.lat, location.coord.lon);
     fetchWeathermapDataFor5Days(location.coord.lat, location.coord.lon);
+    fetchAQI(location.coord.lat, location.coord.lon);
   };
 
   // console.log(locationSearchData);
-  // console.log(weatherData);
-  console.log(fiveDayForecast);
-  console.log(bulkWeatherData);
+  console.log(weatherData);
+  // console.log(fiveDayForecast);
+  // console.log(bulkWeatherData);
+  console.log(aqi);
+  // console.log(timeData);
   return (
     <>
-      <div className="bg-slate-700 w-screen h-screen text-white flex flex-col">
-        {/* left search panel */}
-        <span className="w-3/4 h-1/2 flex bg-slate-800">
+      <div className="bg-slate-800 w-screen h-screen text-white flex flex-col">
+        {/* search panel + current details */}
+        <span className="w-3/4 h-1/3 flex ">
+          {/* search panel */}
           <SearchPanel
             setCityName={setCityName}
             setCountryName={setCountryName}
@@ -97,11 +110,15 @@ export default function App() {
             handleClickLocation={handleClickLocation}
           />
           {/* details of current location */}
-          <CurrentLocationDetails loading={loading} weatherData={weatherData} />
+          <CurrentLocationDetails
+            loading={loading}
+            weatherData={weatherData}
+            aqi={aqi}
+          />
         </span>
 
         {/* middle display panels */}
-        <div className="flex flex-col w-full h-1/2 bg-slate-600  pr-[calc(25%)]">
+        <div className="flex flex-col w-3/4 h-1/2 ">
           {/* details of 5 day forecast */}
           <FiveDayForecastDetails
             bulkWeatherData={bulkWeatherData}
@@ -111,7 +128,7 @@ export default function App() {
           {/* will do later */}
         </div>
         {/* right SOMETHING panel */}
-        <div className="fixed top-0 right-0 w-1/4 h-full bg-slate-500"></div>
+        <div className="fixed top-0 right-0 w-1/4 h-full "></div>
       </div>
     </>
   );
--- src/CurrentLocationDetails.js
+++ src/CurrentLocationDetails.js
@@ -1,16 +1,103 @@
-export default function CurrentLocationDetails({ loading, weatherData }) {
+import { BsSunrise, BsSunset, BsWind } from "react-icons/bs";
+import { FaLocationPin } from "react-icons/fa6";
+import { LuLeaf } from "react-icons/lu";
+export default function CurrentLocationDetails({ loading, weatherData, aqi }) {
+  const windDirection = (degrees) => {
+    const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+    const indexWindComingFrom = Math.round(degrees / 45) % 8;
+    const indexWindGoingTo = (indexWindComingFrom + 4) % 8;
+    return `${directions[indexWindComingFrom]} -> ${directions[indexWindGoingTo]}`;
+  };
   return (
-    <div className="flex w-full h-full border min-h-1/2 flex-shrink-0">
-      {loading && <p>Loading...</p>}
+    <div className="flex w-3/4 h-full  min-h-1/2 flex-shrink-0 px-[calc(5%)] py-[calc(2%)] bg-common">
       {weatherData.coord ? (
-        <span className="flex flex-col">
-          <p>
-            {weatherData.name}, {weatherData.sys.country}
-          </p>
-          <p>Coordinates</p>
-          <p>Latitude: {weatherData.coord.lat}</p>
-          <p>Longitude: {weatherData.coord.lon}</p>
-        </span>
+        <div className="flex flex-col w-full h-full">
+          <span className="flex flex-row justify-between w-full h-full">
+            <div className="flex  w-1/2 items-center bg-common px-2">
+              <span className="flex flex-col w-1/3">
+                <p className="text-base lg:text-lg xl:text-xl 2xl:text-3xl text-wrap ">
+                  {weatherData.name}, {weatherData.sys.country}
+                </p>
+                <span className="flex">
+                  <FaLocationPin />
+                  <p>
+                    {weatherData.coord.lat}, {weatherData.coord.lon}
+                  </p>
+                </span>
+              </span>
+              <span className="flex flex-col justify-center items-center w-2/3">
+                <span className="flex text-3xl lg:text-5xl xl:text-7xl items-center">
+                  <img
+                    src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                    alt="weather"
+                    className="w-16 h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24"
+                  />
+                  <span className="flex">
+                    <p>{Math.round(weatherData.main.temp - 273.15)}</p>
+                    <p className="text-xl"> ℃</p>
+                  </span>
+                </span>
+                <p className="text-base xl:text-xl">
+                  ={weatherData.weather[0].main}=
+                </p>
+                <div className="p-1">
+                  <span className="flex items-center">
+                    <BsSunrise className="text-xl" />
+                    <p className="text-sm xl:text-base">
+                      {new Date(
+                        weatherData.sys.sunrise * 1000
+                      ).toLocaleTimeString()}
+                    </p>
+                  </span>
+                  <span className="flex items-center">
+                    <p className="text-sm xl:text-base">
+                      {new Date(
+                        weatherData.sys.sunset * 1000
+                      ).toLocaleTimeString()}
+                    </p>
+                    <BsSunset className="text-xl" />
+                  </span>
+                </div>
+              </span>
+            </div>
+            {/*  */}
+            <div className="w-0.5 h-full bg-white mx-4" />
+            {/* */}
+            <div className="flex flex-col w-1/2 h-full text-xs lg:text-sm xl:text-base 2xl:text-lg">
+              <div className=" flex justify-between items-center w-full h-2/3">
+                <span className="flex flex-col bg-common w-1/2 h-full m-1 p-2 justify-center">
+                  <span className="flex">
+                    <p>
+                      Feels like:{" "}
+                      {Math.round(weatherData.main.feels_like - 273.15)} ℃
+                    </p>
+                  </span>
+                  <p>Pressure: {weatherData.main.pressure}mbar</p>
+                  <p>Cloudiness: {weatherData.clouds.all}%</p>
+                </span>
+                <span className="flex items-center bg-common m-1 w-1/2 h-full  p-2">
+                  <BsWind className="text-3xl m-1" />
+                  <span>
+                    <p>{windDirection(weatherData.wind.deg)}</p>
+                    <p>{weatherData.wind.speed}</p>
+                  </span>
+                </span>
+              </div>
+              <div className="w-full h-1/3 flex  overflow-x-auto mt-2">
+                <span className="w-2/3  m-1 p-2 flex flex-col justify-center bg-common">
+                  <p className="text-xs text-gray-400">Forecast Description</p>
+                  <p className="text-sm lg:text-base xl:text-lg">
+                    {weatherData.weather[0].description}
+                  </p>
+                </span>
+                <span className="w-1/3  m-1 text-base lg:text-xl xl:text-2xl flex items-center justify-center bg-common">
+                  <p>AQI:{aqi.list[0].main.aqi}</p>
+                  <LuLeaf />
+                </span>
+              </div>
+            </div>
+          </span>
+        </div>
       ) : (
         <p>Search for a location</p>
       )}
