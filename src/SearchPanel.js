86718f06eb1caa2a904ecac1dea560a55801ee40
Debangshu Nath <thecrusader.25225@gmail.com>
2024-07-19 02:03:43 +0530

    update searchpanel

--- src/SearchPanel.js
+++ src/SearchPanel.js
@@ -39,37 +39,38 @@ export default function SearchPanel({
         </div>
       ) : locationSearchData.length !== 0 ? (
         <div className="w-full h-auto flex flex-col overflow-y-auto">
-          {locationSearchData.map((location, index) => (
-            <>
-              <button
-                key={index}
-                className=" min-h-12 hover:bg-white hover:bg-opacity-5 justify-between flex items-center px-2"
-                onClick={() => handleClickLocation(location)}
-              >
-                <p className="text-sm xl:text-lg break-all w-1/2 text-start">
-                  {location?.name}, {location?.sys.country}
-                </p>
-                <span className="flex items-center ">
-                  <img
-                    src={`https://openweathermap.org/img/wn/${location?.weather[0]?.icon}.png`}
-                    alt="weather"
-                    className=""
-                  />
-                  <span className="flex items-start ">
-                    <p className="text-sm xl:text-xl ">
-                      {Math.round(location?.main?.temp - 273.15)}
-                    </p>
-                    <p className="text-[calc(10px)]"> ℃</p>
+          {locationSearchData &&
+            locationSearchData.map((location, index) => (
+              <>
+                <button
+                  key={index}
+                  className=" min-h-12 hover:bg-white hover:bg-opacity-5 justify-between flex items-center px-2"
+                  onClick={() => handleClickLocation(location)}
+                >
+                  <p className="text-sm xl:text-lg break-all w-1/2 text-start">
+                    {location?.name}, {location?.sys.country}
+                  </p>
+                  <span className="flex items-center ">
+                    <img
+                      src={`https://openweathermap.org/img/wn/${location?.weather[0]?.icon}.png`}
+                      alt="weather"
+                      className=""
+                    />
+                    <span className="flex items-start ">
+                      <p className="text-sm xl:text-xl ">
+                        {Math.round(location?.main?.temp - 273.15)}
+                      </p>
+                      <p className="text-[calc(10px)]"> ℃</p>
+                    </span>
                   </span>
-                </span>
-              </button>
-              {index === locationSearchData.length - 1 && (
-                <p className="text-[calc(10px)] text-slate-400 text-center">
-                  Click for details
-                </p>
-              )}
-            </>
-          ))}
+                </button>
+                {index === locationSearchData.length - 1 && (
+                  <p className="text-[calc(10px)] text-slate-400 text-center">
+                    Click for details
+                  </p>
+                )}
+              </>
+            ))}
         </div>
       ) : (
         <NoResults />
