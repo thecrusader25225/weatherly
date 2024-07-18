9a455fbdf3e11e7c362cae8460f63d49ceadfc74
Debangshu Nath <thecrusader.25225@gmail.com>
2024-07-18 21:36:26 +0530

    Update UserLocationData.js

--- src/UserLocationData.js
+++ src/UserLocationData.js
@@ -27,9 +27,7 @@ export default function UserLocationData({
     }
   };
   const fetchWeathermapData = (lat, lon) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-    )
+    fetch(`/api/getWeather?lat=${lat}&lon=${lon}`)
       .then((res) => res.json())
       .then((res) => {
         setUserWeatherData(res);
@@ -37,15 +35,13 @@ export default function UserLocationData({
   };
 
   const fetchAQI = (lat, lon) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-    )
+    fetch(`/api/getAqi?lat=${lat}&lon=${lon}`)
       .then((res) => res.json())
       .then((res) => setUserAqi(res));
   };
 
   const fetchUV = (lat, lon) => {
-    fetch(`https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lon}`, {
+    fetch(`/api/getUv?lat=${lat}&lon=${lon}`, {
       headers: {
         "x-access-token": UV_API_KEY,
       },
